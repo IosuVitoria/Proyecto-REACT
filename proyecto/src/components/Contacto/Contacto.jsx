c91fad1839b6743ed3ca83e01c8e210a4e461d42
Iosu Gómez <131406997+IosuVitoria@users.noreply.github.com>
2023-06-12 19:32:59 +0200

    contacto

--- proyecto/src/components/Contacto/Contacto.jsx
+++ proyecto/src/components/Contacto/Contacto.jsx
@@ -5,14 +5,14 @@ const Contacto = () => {
     <div>
         <h2>Daniela López</h2>
         <h3>Programadora Full Stack developer</h3>
-        <img src="" alt="imagen Daniela" />
+        <img src="./photos/Daniela" alt="imagen Daniela" />
         <p>.......</p>
          {/*Introducir link Github +logo Github
         Perfil linkedin +logo linkedin*/}
 
         <h2>Iosu Gómez</h2>
         <h3>Programadora Full Stack developer</h3>
-        <img src="" alt="imagen Daniela" />
+        <img src="./photos/Iosu.jpg" alt="imagen Iosu" />
         <p>.......</p>
         {/*Introducir link Github +logo Github
         Perfil linkedin +logo linkedin*/}
